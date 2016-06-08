9272b18b41e14c6c68009590f7949ed0672a2728
David Forero <davefogo@gmail.com>
2016-06-08 16:44:28 -0500

    assignment 30:complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,10 @@ posts = Post.all
   )
 end
 
+unique_post = Post.find_or_create_by!(title: "Unique title for a unique post", body: "Uniqu body for a unique post")
+
+unique_post.comments.find_or_create_by(body: "Unique body for a unique comment", post: unique_post)
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
