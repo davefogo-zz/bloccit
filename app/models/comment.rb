5aada35402841f3a0b038047abc9f3313fce7378
David Forero <davefogo@gmail.com>
2016-06-30 12:23:56 -0500

    checkpoint 47: complete

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ActiveRecord::Base
-  after_create :send_favorite_emails
 
   belongs_to :post
   belongs_to :user
@@ -7,11 +6,5 @@ class Comment < ActiveRecord::Base
   validates :body, length: {minimum: 5},presence: true
   validates :user, presence: true
 
-  private
-
-  def send_favorite_emails
-    post.favorites.each do |favorite|
-      FavoriteMailer.new_comment(favorite.user, post, self).deliver_now
-    end
-  end
+  default_scope {order('updated_at DESC')}
 end
